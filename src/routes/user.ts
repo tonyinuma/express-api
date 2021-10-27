04e507f8055a8616fc128f46e8c963ef2bd529d0
Tony ID <tony.inuma@gmail.com>
2021-10-26 22:26:46 -0500

    Add: user routes api and create router folder

---
+++ src/routes/user.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { deleteUser, getUser, getUsers, postUser, putUser } from '../controllers/usersController';
+
+const router = Router();
+
+router.get('/', getUsers)
+router.get('/:id', getUser)
+router.post('/', postUser)
+router.put('/:id', putUser)
+router.delete('/:id', deleteUser)
+
+
+export default router;
